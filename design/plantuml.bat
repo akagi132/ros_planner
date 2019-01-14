2ddfe153328ead4abde83615992a59c3dd78c684
fokko <akagi13213@gmail.com>
2019-01-14 18:19:48 +0900

    class ad

--- design/plantuml.bat
+++ design/plantuml.bat
@@ -1,1 +1,3 @@
-java -jar plantuml.jar sequence.plantuml
+java -jar plantuml.jar sequence.txt
+java -jar plantuml.jar class.txt
+rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen C:\Users\koki\Desktop\git\ros_planner\design\class.png
