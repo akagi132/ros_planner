62adc69787ebf7c009f3a871acb870169859db1e
fokko <akagi13213@gmail.com>
2019-02-17 00:59:19 +0900

    separate class.png

--- design/plantuml.bat
+++ design/plantuml.bat
@@ -1,3 +1,2 @@
-java -jar plantuml.jar sequence.txt
-java -jar plantuml.jar class.txt
-rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen C:\Users\koki\Desktop\git\ros_planner\design\class.png
+java -jar plantuml.jar *.txt
+rem rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen C:\Users\koki\Desktop\git\ros_planner\design\class.png
