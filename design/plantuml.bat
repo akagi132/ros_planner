b90546b57c7ee9f67f92c38d4cc55a3eefb48478
fokko <akagi13213@gmail.com>
2019-02-17 17:01:09 +0900

    class complete sequence drawing

--- design/plantuml.bat
+++ design/plantuml.bat
@@ -1,2 +1,2 @@
-java -jar plantuml.jar *.txt
+java -jar plantuml.jar sequence.txt
 rem rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen C:\Users\koki\Desktop\git\ros_planner\design\class.png
